0c86f74e18954e3f7d13c88dda351ee9f842ba3a
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2025-03-25 09:19:55 -0400

    update request details to match designs and add svg images (#38)

--- packages/permissions-provider-snap/src/ui/components/index.ts
+++ packages/permissions-provider-snap/src/ui/components/index.ts
@@ -1,5 +1,5 @@
 export * from './header';
-export * from './request-details';
+export * from './RequestDetails';
 export * from './account-details';
 export * from './spending-cap-details';
 export * from './pagination';
