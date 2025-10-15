91f26d5a1f72d61f0e97f03dabf9aac6056ca537
MoMannn <tadej.vengust@consensys.net>
2025-10-15 11:48:45 +0200

    rename header title and subtitle

--- packages/gator-permissions-snap/snap.manifest.ts
+++ packages/gator-permissions-snap/snap.manifest.ts
@@ -12,7 +12,7 @@ const snapEnv = process.env.SNAP_ENV;
 const manifest: SnapManifest = {
   version: packageJson.version,
   description: 'Grants 7715 permissions from a DeleGator smart account',
-  proposedName: 'Gator Permissions',
+  proposedName: 'Permission Request',
   repository: {
     type: 'git',
     url: 'https://github.com/MetaMask/snap-7715-permissions.git',
